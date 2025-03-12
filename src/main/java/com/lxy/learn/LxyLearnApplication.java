31a880cec0ed7c39397bcc67dac31fe3428d7cc1
宋胜平 <546636494@qq.com>
2025-03-12 21:04:21 +0800

    first commit

---
+++ src/main/java/com/lxy/learn/LxyLearnApplication.java
@@ -0,0 +1,13 @@
+package com.lxy.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxyLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LxyLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lxy/learn/LxyLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxy.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxyLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
